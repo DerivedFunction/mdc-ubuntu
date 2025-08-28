9cf6af9aafe2d53e2c828368d8ede5e4b39e55c5
Denny Li <ytdli08@gmail.com>
2025-08-28 06:18:01 +0000

    update

--- .devcontainer/intitalSetup.sh
+++ .devcontainer/intitalSetup.sh
@@ -4,7 +4,3 @@ apt install vim
 # for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# remember to add to ~/.bashrc 
-# source /opt/ros/humble/setup.bash
-   
