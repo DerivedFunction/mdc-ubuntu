76bada3ae791a1c3cfaa8ddcfc5634677eb9de32
Denny Li <ytdli08@gmail.com>
2025-09-04 20:25:03 +0000

    test

--- src/laptop/setup.py
+++ src/laptop/setup.py
@@ -4,24 +4,24 @@ package_name = 'laptop'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='thong',
-    maintainer_email='tdv225@lehigh.edu',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="thong",
+    maintainer_email="tdv225@lehigh.edu",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'input_node = laptop.input:main',
-            'listener = laptop.listener:main',
+        "console_scripts": [
+            "input_node = laptop.input:main",
+            "listener = laptop.listener:main",
+            "velocity_to_controller = laptop.velocity_to_controller:main",
         ],
     },
 )
