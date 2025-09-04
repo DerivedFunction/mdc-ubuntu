eb620f83d1f795f2df39e879438b4c8d31af06f5
mdc-capstone <lehighmdc@gmail.com>
2025-09-04 17:59:13 -0400

    rename folder

--- .rtabmap_data/viewMap.sh
+++
@@ -1,7 +0,0 @@
-# Run the db viewer
-# What you can do inside:
-# 3D Map View: Visualize point clouds or meshes from your saved sessions.
-# Graph View: See the pose graph (nodes are your robot’s positions, edges are loop closures).
-# Images / Scans: Inspect the raw RGB, depth, and laser scans stored in each node.
-# Export: You can export trajectories, point clouds, or meshes for use in RViz or other tools.
-rtabmap-databaseViewer rtabmap.db
