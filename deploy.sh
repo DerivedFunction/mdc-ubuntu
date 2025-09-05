e41c04726961c30705357eb030de5ff488eb0886
mdc-capstone <lehighmdc@gmail.com>
2025-09-05 11:14:02 -0400

    update to ignore files

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-scp -Cr ../mdc-ubuntu capstone@mdc-nx.local:/home/capstone/mdc-ubuntu
+# scp -Cr ../mdc-ubuntu capstone@mdc-nx.local:/home/capstone/mdc-ubuntu
+rsync -avz --filter=':- .gitignore' \
+    --exclude='.git' \
+    --exclude='*.db' \
+    ../mdc-ubuntu capstone@mdc-nx.local:/home/capstone/
